69b45d77afad3a5b353ea9fb74713aeb34e61c35
PBH <parkbo1120@naver.com>
2026-06-09 16:40:59 +0900

    패키지 구조 정리

--- src/main/java/com/practice/backend/Controller/TestController.java
+++ src/main/java/com/practice/backend/Controller/TestController.java
@@ -1,4 +1,4 @@
-package com.practice.backend.Controller;
+package com.practice.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
